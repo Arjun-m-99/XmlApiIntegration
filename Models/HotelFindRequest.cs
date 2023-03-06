1dcb61e32e67e94f29db768064e68519e4bd4fdd
MallikarjunaRudraniTech <121670751+MallikarjunaRudraniTech@users.noreply.github.com>
2023-03-06 11:53:51 +0530

    tried with auto model class for HotelFindRequest class

--- Models/HotelFindRequest.cs
+++ Models/HotelFindRequest.cs
@@ -1,10 +1,275 @@
 ﻿namespace RezLiveApis.Models
 {
-    public class HotelFindRequest
+    //public class HotelFindRequest
+    //{
+    //    public Authentication Authentication { get; set; }
+    //    public Booking Booking { get; set; }
+    //}
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class HotelFindRequest
     {
-        public Authentication Authentication { get; set; }
-        public Booking Booking { get; set; }
+
+        private HotelFindRequestAuthentication authenticationField;
+
+        private HotelFindRequestBooking bookingField;
+
+        /// <remarks/>
+        public HotelFindRequestAuthentication Authentication
+        {
+            get
+            {
+                return this.authenticationField;
+            }
+            set
+            {
+                this.authenticationField = value;
+            }
+        }
+
+        /// <remarks/>
+        public HotelFindRequestBooking Booking
+        {
+            get
+            {
+                return this.bookingField;
+            }
+            set
+            {
+                this.bookingField = value;
+            }
+        }
     }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class HotelFindRequestAuthentication
+    {
+
+        private string agentCodeField;
+
+        private string userNameField;
+
+        private string passwordField;
+
+        /// <remarks/>
+        public string AgentCode
+        {
+            get
+            {
+                return this.agentCodeField;
+            }
+            set
+            {
+                this.agentCodeField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string UserName
+        {
+            get
+            {
+                return this.userNameField;
+            }
+            set
+            {
+                this.userNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Password
+        {
+            get
+            {
+                return this.passwordField;
+            }
+            set
+            {
+                this.passwordField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class HotelFindRequestBooking
+    {
+
+        private string arrivalDateField;
+
+        private string departureDateField;
+
+        private string countryCodeField;
+
+        private ushort cityField;
+
+        private string guestNationalityField;
+
+        private byte[] hotelRatingsField;
+
+        private HotelFindRequestBookingRoom[] roomsField;
+
+        /// <remarks/>
+        public string ArrivalDate
+        {
+            get
+            {
+                return this.arrivalDateField;
+            }
+            set
+            {
+                this.arrivalDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string DepartureDate
+        {
+            get
+            {
+                return this.departureDateField;
+            }
+            set
+            {
+                this.departureDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string CountryCode
+        {
+            get
+            {
+                return this.countryCodeField;
+            }
+            set
+            {
+                this.countryCodeField = value;
+            }
+        }
+
+        /// <remarks/>
+        public ushort City
+        {
+            get
+            {
+                return this.cityField;
+            }
+            set
+            {
+                this.cityField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string GuestNationality
+        {
+            get
+            {
+                return this.guestNationalityField;
+            }
+            set
+            {
+                this.guestNationalityField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayAttribute()]
+        [System.Xml.Serialization.XmlArrayItemAttribute("HotelRating", IsNullable = false)]
+        public byte[] HotelRatings
+        {
+            get
+            {
+                return this.hotelRatingsField;
+            }
+            set
+            {
+                this.hotelRatingsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Room", IsNullable = false)]
+        public HotelFindRequestBookingRoom[] Rooms
+        {
+            get
+            {
+                return this.roomsField;
+            }
+            set
+            {
+                this.roomsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class HotelFindRequestBookingRoom
+    {
+
+        private string typeField;
+
+        private byte noOfAdultsField;
+
+        private byte noOfChildsField;
+
+        /// <remarks/>
+        public string Type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte NoOfAdults
+        {
+            get
+            {
+                return this.noOfAdultsField;
+            }
+            set
+            {
+                this.noOfAdultsField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte NoOfChilds
+        {
+            get
+            {
+                return this.noOfChildsField;
+            }
+            set
+            {
+                this.noOfChildsField = value;
+            }
+        }
+    }
+
+
 
     public class Booking
     {
